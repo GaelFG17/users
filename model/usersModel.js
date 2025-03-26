91baf2b18d62c410648682d3d13c9e12cdc4d9a5
GaelFG17 <gaelfg1720@gmail.com>
2025-03-25 19:16:00 -0600

    concepto de modelo para usuarios

---
+++ model/usersModel.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name :{String, required: true},
+    email :{String, required: true, unique : true},
+    password : {String, required: true},
+    role : {String, required: true},
+    status : {String, required: true},
+})
