252f54dcef24ee958973100647ea8ef6dffe11ce
GaelFG17 <gaelfg1720@gmail.com>
2025-03-25 19:39:28 -0600

    metodo para buscar todo

--- service/usersService.js
+++ service/usersService.js
@@ -1,5 +1,18 @@
 const {Users} = require('../model/usersModel')
 
 class UsersService{
-    
+
+    static async getUsers(){
+        try{
+            const users = await Users.find()
+            if (users.length > 0){
+                return {status: 200, data : users}
+            }else{
+                return {status: 404, message : "Users not found"}
+            }
+        }catch(error){
+            return {status: 500, message : error}
+        }
+    }
+
 }
