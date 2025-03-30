ae852d8104eefe05f0e8245abc6bf610a766f486
GaelFG17 <gaelfg1720@gmail.com>
2025-03-30 14:16:26 -0600

    controller para obtener todos los usuarios

--- controller/userController.js
+++ controller/userController.js
@@ -3,7 +3,12 @@ const {UsersService} = require('../service/usersService')
 class UsersController{
 
     static async getUsers(req, res){
-        const response = await UsersService.
+        const response = await UsersService.getUsers()
+        if (response.status){
+            res.status(response.status).json({message: response.message})
+        }else{
+            res.status(200).json(response.data)
+        }
     }
 
 
